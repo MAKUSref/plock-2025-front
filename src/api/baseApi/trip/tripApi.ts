618e8a05c39ba098ba8f60d690250ece382b58cf
Bartosz Palewicz <bartosz.palewicz0@gmail.com>
2025-10-19 06:52:17 +0200

    adding trip api

---
+++ src/api/baseApi/trip/tripApi.ts
@@ -0,0 +1,54 @@
+import type { GenerateTripData, Trip } from "./types";
+import { baseApi } from "../baseApi";
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllTrips: builder.query<void, void>({
+      query: () => ({
+        url: "/trips",
+        method: "GET",
+      }),
+    }),
+    getTripById: builder.query<void, { tripId: string }>({
+      query: ({ tripId }) => ({
+        url: `/trips/${tripId}`,
+        method: "GET",
+      }),
+    }),
+    createTrip: builder.mutation<void, Trip>({
+      query: () => ({
+        url: "/trips",
+        method: "POST",
+      }),
+    }),
+    generateTrip: builder.mutation<void, GenerateTripData>({
+      query: (data) => ({
+        url: "/trips/generate",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateTrip: builder.mutation<void, Trip>({
+      query: (trip) => ({
+        url: `/trips/${trip.id}`,
+        method: "PUT",
+        body: trip,
+      }),
+    }),
+    deleteTrip: builder.mutation<void, { tripId: string }>({
+      query: ({ tripId }) => ({
+        url: `/trips/${tripId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllTripsQuery,
+  useGetTripByIdQuery,
+  useCreateTripMutation,
+  useGenerateTripMutation,
+  useUpdateTripMutation,
+  useDeleteTripMutation,
+} = authApi;
