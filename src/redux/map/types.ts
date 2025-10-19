db52bf569a9c55494d50ec16cbd2af92a3bf5868
MAKUSref <tomaszmakowski002731@gmail.com>
2025-10-19 04:54:49 +0200

    add routes details

--- src/redux/map/types.ts
+++ src/redux/map/types.ts
@@ -1,14 +1,21 @@
-import type { Coordinate } from "../../api/navigationApi/types";
+import type { Coordinate, Step } from "../../api/navigationApi/types";
 
 export type Location = {
   location: Coordinate;
   name: string;
 }
 
+export type RouteData = {
+  type: "pedestrian" | "cycling";
+  distance: number;
+  duration: number;
+  steps: Step[];
+}
+
 export type Route = {
   id: string;
-  isActive: boolean;
-  data?: GeoJSON.FeatureCollection;
+  state: "idle" | "pending" | "done";
+  details?: RouteData;
 }
 
 export type MapState = {
